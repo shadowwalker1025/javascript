22243bf0b51f955316645102a4deb7d962378a61
shaowwalker1025 <nagendra.tere@gmail..com>
2021-12-21 23:10:31 +0530

    first commit

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function fact() {
+   // const num = parseInt(prompt('Enter an integer: '));
+    let fact = 1 ,num;
+    for (i=1;i<=num;i++){
+        fact = fact * i;
+    }
+    console.log(`the factorial of ${num} is ${fact}.`)
+}
+fact();
